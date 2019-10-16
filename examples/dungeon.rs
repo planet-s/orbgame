c9c8f402abbd35b4937576ce32ad5743907683a3
Florian Blasius <flovanpt@posteo.de>
2019-10-16 08:32:47 +0200

    Finish view change on dugeon example.

--- examples/dungeon.rs
+++ examples/dungeon.rs
@@ -1,6 +1,9 @@
 use orbgame::prelude::*;
 use orbgame::theme::DEFAULT_THEME_CSS;
-use std::{cell::{Cell, RefCell}, rc::Rc};
+use std::{
+    cell::{Cell, RefCell},
+    rc::Rc,
+};
 
 static DUNGEON_THEME: &'static str = include_str!("res/dungeon/theme.css");
 
@@ -10,38 +13,13 @@ fn get_theme() -> ThemeValue {
         .build()
 }
 
-#[derive(Copy, Clone)]
-pub enum MapViewAction {
-    Start,
-}
-
 #[derive(Default, Clone)]
-pub struct MapViewState {
-    action: Cell<Option<MapViewAction>>,
-}
+pub struct MapViewState {}
 
-impl MapViewState {
-    pub fn action(&self, action: MapViewAction) {
-        self.action.set(Some(action));
-    }
-}
-
-impl State for MapViewState {
-    fn update(&self, ctx: &mut Context<'_>) {
-        if let Some(action) = self.action.get() {
-            match action {
-                MapViewAction::Start => {
-                    ctx.widget().set::<Visibility>(Visibility::from("Visible"));
-                }
-            }
-
-            self.action.set(None);
-        }
-    }
-}
+impl State for MapViewState {}
 
 widget!(MapView<MapViewState> {
-
+     selector: Selector
 });
 
 impl Template for MapView {
@@ -63,32 +41,25 @@ pub enum MenuAction {
 #[derive(Default)]
 pub struct MenuViewState {
     action: Cell<Option<MenuAction>>,
-    map_view_state: RefCell<Option<Rc<MapViewState>>>,
 }
 
 impl MenuViewState {
     fn action(&self, action: MenuAction) {
         self.action.set(Some(action));
     }
-
-    fn set_map_view_state(&self, state: Rc<MapViewState>) {
-        *self.map_view_state.borrow_mut() = Some(state);
-    }
 }
 
 impl State for MenuViewState {
     fn update(&self, ctx: &mut Context<'_>) {
         if let Some(action) = self.action.get() {
-            match action {
-                MenuAction::Start => {
-                    ctx.widget()
-                        .set::<Visibility>(Visibility::from("collapsed"));
-                    if let Some(state) = &*self.map_view_state.borrow() {
-                        state.action(MapViewAction::Start);
+            if let Some(window_id) = ctx.parent_entity_by_element("window") {
+                match action {
+                    MenuAction::Start => {
+                        ctx.push_event_by_entity(GameEvent::StartGame, window_id);
                     }
-                }
-                MenuAction::Quit => {
-                    ctx.push_event(SystemEvent::Quit);
+                    MenuAction::Quit => {
+                        ctx.push_event_by_entity(GameEvent::Quit, window_id);
+                    }
                 }
             }
 
@@ -109,80 +80,152 @@ impl Template for MenuView {
         let ng_state = state.clone();
         let q_state = state.clone();
 
-        self.name("MenuView")
-            .selector(Selector::default().id("menu_view"))
-            .child(
-                Grid::create()
-                    .selector(Selector::from("grid").class("start"))
-                    .child(
-                        Container::create()
-                            .padding(16.0)
-                            .selector(Selector::from("container").class("menu"))
-                            .vertical_alignment("Center")
-                            .horizontal_alignment("Center")
-                            .child(
-                                Stack::create()
-                                    .child(
-                                        TextBlock::create()
-                                            .selector(Selector::from("textblock").class("h1"))
-                                            .text("Dungeon")
-                                            .horizontal_alignment("Center")
-                                            .build(ctx),
-                                    )
-                                    .child(
-                                        Button::create()
-                                            .margin((0.0, 16.0, 0.0, 0.0))
-                                            .text("Start Game")
-                                            .on_click(move |_| {
-                                                ng_state.action(MenuAction::Start);
-                                                true
-                                            })
-                                            .build(ctx),
-                                    )
-                                    .child(
-                                        Button::create()
-                                            .margin((0.0, 8.0, 0.0, 0.0))
-                                            .text("Quit")
-                                            .on_click(move |_| {
-                                                q_state.action(MenuAction::Quit);
-                                                true
-                                            })
-                                            .build(ctx),
-                                    )
-                                    .build(ctx),
-                            )
-                            .build(ctx),
-                    )
-                    .build(ctx),
-            )
+        self.name("MenuView").child(
+            Grid::create()
+                .selector(Selector::from("grid").class("start"))
+                .child(
+                    Container::create()
+                        .padding(16.0)
+                        .selector(Selector::from("container").class("menu"))
+                        .vertical_alignment("Center")
+                        .horizontal_alignment("Center")
+                        .child(
+                            Stack::create()
+                                .child(
+                                    TextBlock::create()
+                                        .selector(Selector::from("textblock").class("h1"))
+                                        .text("Dungeon")
+                                        .horizontal_alignment("Center")
+                                        .build(ctx),
+                                )
+                                .child(
+                                    Button::create()
+                                        .margin((0.0, 16.0, 0.0, 0.0))
+                                        .text("Start Game")
+                                        .on_click(move |_| {
+                                            ng_state.action(MenuAction::Start);
+                                            true
+                                        })
+                                        .build(ctx),
+                                )
+                                .child(
+                                    Button::create()
+                                        .margin((0.0, 8.0, 0.0, 0.0))
+                                        .text("Quit")
+                                        .on_click(move |_| {
+                                            q_state.action(MenuAction::Quit);
+                                            true
+                                        })
+                                        .build(ctx),
+                                )
+                                .build(ctx),
+                        )
+                        .build(ctx),
+                )
+                .build(ctx),
+        )
     }
 }
-widget!(
-    GameView {
-        selector: Selector
+
+#[derive(Copy, Clone)]
+pub enum GameEvent {
+    StartGame,
+    Quit,
+}
+
+pub type GameHandlerFn = dyn Fn(&GameEvent) -> bool + 'static;
+
+pub struct GameEventHandler {
+    handler: Rc<GameHandlerFn>,
+}
+
+impl Into<Rc<dyn EventHandler>> for GameEventHandler {
+    fn into(self) -> Rc<dyn EventHandler> {
+        Rc::new(self)
     }
-);
+}
+
+impl EventHandler for GameEventHandler {
+    fn handle_event(&self, event: &EventBox) -> bool {
+        if let Ok(event) = event.downcast_ref::<GameEvent>() {
+            return (self.handler)(event);
+        }
+
+        return false;
+    }
+
+    fn handles_event(&self, event: &EventBox) -> bool {
+        event.is_type::<GameEvent>()
+    }
+}
+
+impl Event for GameEvent {}
+
+#[derive(Default, Clone)]
+pub struct GameViewState {
+    event: Cell<Option<GameEvent>>,
+}
+
+impl GameViewState {
+    fn game_event(&self, event: &GameEvent) {
+        self.event.set(Some(*event));
+    }
+}
+
+impl State for GameViewState {
+    fn update(&self, ctx: &mut Context<'_>) {
+        if let Some(event) = self.event.get() {
+            match event {
+                GameEvent::StartGame => {
+                    ctx.child_by_id("menu_view").unwrap()
+                        .set::<Visibility>(Visibility::from("collapsed"));
+                     ctx.child_by_id("map_view").unwrap()
+                        .set::<Visibility>(Visibility::from("visible"));
+                }
+                GameEvent::Quit => {
+                    ctx.push_event(SystemEvent::Quit);
+                }
+            }
+
+            self.event.set(None);
+        }
+    }
+}
+
+widget!(GameView<GameViewState> { selector: Selector });
+
+impl GameView {
+    fn on_game_event<H: Fn(&GameEvent) -> bool + 'static>(self, handler: H) -> Self {
+        self.insert_handler(GameEventHandler {
+            handler: Rc::new(handler),
+        })
+    }
+}
 
 impl Template for GameView {
     fn template(self, _: Entity, ctx: &mut BuildContext) -> Self {
-        let map_view = MapView::create();
-        let map_view_state = map_view.clone_state();
-        let menu_view = MenuView::create();
-        let menu_view_state = menu_view.clone_state();
-        menu_view_state.set_map_view_state(map_view_state);
-
+        let state = self.clone_state();
         self.name("GameView")
             .selector(Selector::default().id("game_view"))
             .child(
                 Grid::create()
                     .child(
-                        map_view
+                        MapView::create()
+                          .selector(Selector::default().id("map_view"))
                             .visibility(Visibility::from("collapsed"))
                             .build(ctx),
                     )
-                    .child(menu_view.build(ctx))
+                    .child(
+                        MenuView::create()
+                            .selector(Selector::default().id("menu_view"))
+                            .build(ctx),
+                    )
                     .build(ctx),
             )
+            .on_game_event(move |e| {
+                state.game_event(e);
+                true
+            })
     }
 }
 
